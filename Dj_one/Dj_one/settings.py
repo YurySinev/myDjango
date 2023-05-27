904032b10eb1d8380c1874f1cfa3fd076b98b614
Yury Sinev <yuraseg@gmail.com>
2023-05-28 01:42:46 +0300

    v3.1

--- Dj_one/Dj_one/settings.py
+++ Dj_one/Dj_one/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages'
 ]
 
 SITE_ID = 1
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
